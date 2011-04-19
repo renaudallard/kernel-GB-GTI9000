01809f36ae599ac94799efb98eb59be7ffdd1bd4
Renaud Allard <renaud@allard.it>
2011-04-19 16:57:32 +0200

    Initial commits after change of root in kernel tree

---
+++ fast_compile_kernel.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+. ./setenv.sh
+chmod 4755 ~/galaxy/initramfs-gb/root/sbin/su
+make
+find . -name *.ko | while read MODULE; do cp $MODULE ../initramfs-gb/root/lib/modules/ ; done
+make -j4
+cp arch/arm/boot/zImage /home/public_html/r/
