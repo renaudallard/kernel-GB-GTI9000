4dd4b6a007dca0f8d37a894efb5e86f13ddf7410
Renaud Allard <renaud@allard.it>
2011-04-22 11:14:53 +0200

    Change toolchain Change initramfs compression Use preemptible RCU

--- setenv.sh
+++ setenv.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 export ARCH=arm 
-export PATH=$PATH:/home/r/arm-2009q3/bin:/usr/java/jdk1.6.0_21/bin
-export CROSS_COMPILE=/home/r/arm-2009q3/bin/arm-none-eabi-
+export PATH=$PATH:/home/r/arm-2010q1/bin:/usr/java/jdk1.6.0_21/bin
+export CROSS_COMPILE=/home/r/arm-2010q1/bin/arm-none-eabi-
 #export ac_cv_linux_vers=2.6.32
-export CC=/home/r/arm-2009q3/bin/arm-none-eabi-gcc
+export CC=/home/r/arm-2010q1/bin/arm-none-eabi-gcc
 #export CC=/home/r/galaxy/agcc
-export LD=/home/r/arm-2009q3/bin/arm-none-eabi-ld
-export AR=/home/r/arm-2009q3/bin/arm-none-eabi-ar
-export RANLIB=/home/r/arm-2009q3/arm-none-eabi/bin/ranlib
-export PATH=$PATH:/home/r/arm-2009q3/bin
-export PATH=$PATH:/home/r/arm-2009q3/
-export PATH=$PATH:/home/r/arm-2009q3/lib
+export LD=/home/r/arm-2010q1/bin/arm-none-eabi-ld
+export AR=/home/r/arm-2010q1/bin/arm-none-eabi-ar
+export RANLIB=/home/r/arm-2010q1/arm-none-eabi/bin/ranlib
+export PATH=$PATH:/home/r/arm-2010q1/bin
+export PATH=$PATH:/home/r/arm-2010q1/
+export PATH=$PATH:/home/r/arm-2010q1/lib
 #export CFLAGS="-static -Os -mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -fno-gcse -fprefetch-loop-arrays --param l2-cache-size=512 --param l1-cache-size=64 --param simultaneous-prefetches=6 --param prefetch-latency=400 --param l1-cache-line-size=64"
 export CFLAGS="-static -Os -fstack-protector -fstack-protector-all"
 export LDFLAGS="-static -Os"
