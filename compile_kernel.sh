0e16ff908324eadfe19f09a77e75adaa976e798a
Renaud Allard <renaud@allard.it>
2011-04-25 08:41:45 +0200

    Config sync

--- compile_kernel.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-. ./setenv.sh
-make -i clean
-chmod 4755 ~/galaxy/initramfs-gb/root/sbin/su
-make -j4
-find . -name *.ko | while read MODULE; do cp $MODULE ../initramfs-gb/root/lib/modules/ ; done
-make -j4
-cp arch/arm/boot/zImage /home/public_html/r/
